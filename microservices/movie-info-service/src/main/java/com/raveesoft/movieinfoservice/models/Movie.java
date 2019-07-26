caecbbc072d78bfd9bfa4f40096027d2a6c433c6
P0102475 <P0102475@NCS-021018JJ09.ncs.corp.int-ads>
2019-07-26 16:14:12 +0800

    Add Controlle - Movie Info Service

---
+++ microservices/movie-info-service/src/main/java/com/raveesoft/movieinfoservice/models/Movie.java
@@ -0,0 +1,33 @@
+package com.raveesoft.movieinfoservice.models;
+
+public class Movie {
+	
+	private String name;
+	private String desc;
+	
+	public Movie(String name, String desc) {
+		super();
+		this.name = name;
+		this.desc = desc;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
+	
+	
+}
---
+++ microservices/movie-info-service/src/main/java/com/raveesoft/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,19 @@
+package com.raveesoft.movieinfoservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.raveesoft.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable String movieId) {
+		return new Movie("Avatar","Test");
+		
+	}
+
+}
